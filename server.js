408b1fed7aeeb619b82e9e0f56f7003ddc329b69
Khawar Ali <khawarali971@gmail.com>
2016-07-21 22:55:46 +0500

    update todo api

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 var express = require('express')
+var bodyparser = require('body-parser')
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-        id: 1,
-        description: 'meet him at 1',
-        completed: false
-},
-    {
-        id: 2,
-        description: 'go to market',
-        completed: false
-},
-    {
-        id: 3,
-        description: 'go to petrol pump',
-        completed: true
-}]
+var todos = []
+var todoNextId = 1
+app.use(bodyparser.json());
 app.get('/', function (req, res) {
     res.send('Todo API Root')
 })
@@ -23,19 +12,24 @@ app.get('/todos', function (req, res) {
     res.json(todos)
 })
 app.get('/todos/:id', function (req, res) {
-    var todoid = parseInt(req.params.id,10);
+    var todoid = parseInt(req.params.id, 10);
     var matchedTodo;
-    todos.forEach(function(todo){
-        if(todoid === todo.id){
+    todos.forEach(function (todo) {
+        if (todoid === todo.id) {
             matchedTodo = todo;
         }
     })
-    if(matchedTodo){
+    if (matchedTodo) {
         res.json(matchedTodo)
-    }
-    else{
+    } else {
         res.status(404).send();
     }
+})
+app.post('/todos', function (req, res) {
+ var body = req.body
+ body.id = todoNextId++
+ todos.push(body)
+ res.json(body)
 })
 app.listen(PORT, function () {
     console.log('server running on ' + PORT)
