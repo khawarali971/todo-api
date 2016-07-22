e545a28cf3a4451ca420f067f7ea382e86964077
Khawar Ali <khawarali971@gmail.com>
2016-07-22 07:01:01 +0500

    TODO API DELETE function

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ app.get('/todos', function (req, res) {
 })
 app.get('/todos/:id', function (req, res) {
     var todoid = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id : todoid})
-   
+    var matchedTodo = _.findWhere(todos, {
+        id: todoid
+    })
+
     if (matchedTodo) {
         res.json(matchedTodo)
     } else {
@@ -23,14 +25,28 @@ app.get('/todos/:id', function (req, res) {
     }
 })
 app.post('/todos', function (req, res) {
- var body = _.pick(req.body, 'description','completed' )
- if(!_.isString(body.description) || !_.isBoolean(body.completed) || body.description.trim().length === 0){
-     return res.status(400).send();
- }
-   body.description = body.description.trim()
- body.id = todoNextId++
- todos.push(body)
- res.json(body)
+    var body = _.pick(req.body, 'description', 'completed')
+    if (!_.isString(body.description) || !_.isBoolean(body.completed) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+    body.description = body.description.trim()
+    body.id = todoNextId++
+        todos.push(body)
+    res.json(body)
+})
+app.delete('/todos/:id', function (req, res) {
+    var todoid = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {
+        id: todoid
+    })
+    if (matchedTodo) {
+        todos = _.without(todos, matchedTodo);
+        //res.status(200).json('deleted id ' + todoid);
+        res.json(matchedTodo)
+    } else {
+        res.status(404).json('error: todo id not found');
+    }
+
 })
 app.listen(PORT, function () {
     console.log('server running on ' + PORT)
