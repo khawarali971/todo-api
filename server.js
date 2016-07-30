b676550f61624708995f49b76c1ab77ec6690a51
khawar ali <khawarali971@gmail.com>
2016-07-31 00:25:35 +0500

    update model

--- server.js
+++ server.js
@@ -132,28 +132,30 @@ app.delete('/todos/:id', function (req, res) {
 
 app.put('/todos/:id', function (req, res) {
     var todoid = parseInt(req.params.id, 10)
-    var matchtodo = _.findWhere(todos, {
-        id: todoid
-    })
+    
     var body = _.pick(req.body, 'description', 'completed')
-    var validattributes = {};
-    if (!matchtodo) {
-        res.send(404).send();
-    }
+    var attributes = {};
+   
 
-
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validattributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        res.send(400).send();
-    }
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-        validattributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        res.send(400).send();
-    }
-    _.extend(matchtodo, validattributes)
-    res.json(matchtodo)
+    if (body.hasOwnProperty('completed') ) {
+        attributes.completed = body.completed;
+    } 
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
+    } 
+    db.todo.findById(todoid).then(function(todo){
+        if(todo){
+            todo.update(attributes).then(function(todo){
+            res.json(todo.toJSON())
+        },function(e){
+                res.status(400).json(e)
+            })
+        }else{
+            res.status(404).send()
+        }
+    }, function(){
+        res.status(500).send()
+    })
 })
 db.sequalize.sync().then(function () {
     app.listen(PORT, function () {
