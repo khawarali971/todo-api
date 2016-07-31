064289db1cf54e2e85ca8537f41cd8986cf11432
khawar ali <khawarali971@gmail.com>
2016-07-31 13:53:38 +0500

    add user model

--- db.js
+++ db.js
@@ -12,7 +12,9 @@ if (env === 'production') {
     });
 }
 var db = {};
+
 db.todo = sequelize.import(__dirname + '/models/todo.js')
+db.user = sequelize.import(__dirname + '/models/user.js')
 db.sequalize = sequelize;
 db.Sequalize = Sequelize;
 
--- server.js
+++ server.js
@@ -13,147 +13,102 @@ app.get('/', function (req, res) {
 app.get('/todos', function (req, res) {
     var query = req.query;
     var where = {};
-    if (query.hasOwnProperty('completed') && query.completed === 'true'){
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
         where.completed = true
-    }  
-    else if (query.hasOwnProperty('completed') && query.completed === 'false'){
+    } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
         where.completed = false
-    } 
+    }
     if (query.hasOwnProperty('q') && query.q.length > 0) {
         where.description = {
             $like: '%' + query.q + '%'
         }
-        }
+    }
     db.todo.findAll({
-                where: where
-                }).then(function(todos){
-      res.json(todos)
-        }, function (e) {
-            res.status(500).json(e)
-        })
-    /*var filteredtools = todos;
-    var queryparams = req.query;
-    if (queryparams.hasOwnProperty('completed') && queryparams.completed === 'true') {
-        filteredtools = _.where(filteredtools, {
-            completed: true
-        })
+        where: where
+    }).then(function (todos) {
+        res.json(todos)
+    }, function (e) {
+        res.status(500).json(e)
+    })
 
-    } else if (queryparams.hasOwnProperty('completed') && queryparams.completed === 'false') {
-        filteredtools = _.where(filteredtools, {
-            completed: false
-        })
-    }
-    if (queryparams.hasOwnProperty('q') && queryparams.q.length > 0) {
-        filteredtools = _.filter(filteredtools, function (todo) {
-            return todo.description.toLowerCase().indexOf(queryparams.q.toLowerCase()) > -1;
-        })
-    }
-    res.json(filteredtools)*/
 })
 app.get('/todos/:id', function (req, res) {
     var todoid = parseInt(req.params.id, 10);
-    db.todo.findById(todoid).then(function(todo){
-        if(!!todo){
-             res.json(todo.toJSON())
-        }else{
+    db.todo.findById(todoid).then(function (todo) {
+        if (!!todo) {
+            res.json(todo.toJSON())
+        } else {
             res.status(404).send()
         }
-    }, function(e){
-            res.status(500).send()
-        }) 
-    /* db.todo.findAll({
-        where: {
-            id: todoid
-        }
-    }).then(function(todos){
-        if(todos){
-            return todos.forEach(function (todo) {
-                  res.json(todo.toJSON()) })
-        }
-        else{
-            res.status(500).send()
-        }
-     })*/
-  /*  var matchedTodo = _.findWhere(todos, {
-        id: todoid
+    }, function (e) {
+        res.status(500).send()
     })
 
-    if (matchedTodo) {
-        res.json(matchedTodo)
-    } else {
-        res.status(404).send();
-    }*/
 })
 
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed')
     db.todo.create(body).then(function (todo) {
-            res.json(todo.toJSON())
-        }, function (e) {
-            res.status(404).json(e)
-        }).catch(function(e){
+        res.json(todo.toJSON())
+    }, function (e) {
+        res.status(404).json(e)
+    }).catch(function (e) {
         console.log(e)
     })
-        /*if (!_.isString(body.description) || !_.isBoolean(body.completed) || body.description.trim().length === 0) {
-            return res.status(400).send();
-        }
-        body.description = body.description.trim()
-        body.id = todoNextId++
-            todos.push(body)
-        res.json(body)*/
+
+})
+app.post('/users', function(req,res){
+    var body  = _.pick(req.body, 'email', 'password')
+    db.user.create(body).then(function (user) {
+        res.json(user.toJSON())
+    }, function (e) {
+        res.status(404).json(e)
+    }).catch(function (e) {
+        console.log(e)
+    })
 })
 app.delete('/todos/:id', function (req, res) {
     var todoid = parseInt(req.params.id, 10);
     db.todo.destroy({
-        where:{
-            id : todoid
+        where: {
+            id: todoid
         }
-    }).then(function(rowDeleted){
-        if(rowDeleted === 0){
+    }).then(function (rowDeleted) {
+        if (rowDeleted === 0) {
             res.status(404).json('no record deleted')
-        }else{
+        } else {
             res.status(204).send()
         }
     }, function () {
-            res.status(500).send()
-        })
-    /*var matchedTodo = _.findWhere(todos, {
-        id: todoid
+        res.status(500).send()
     })
-    if (matchedTodo) {
-        todos = _.without(todos, matchedTodo);
-        //res.status(200).json('deleted id ' + todoid);
-        res.json(matchedTodo)
-    } else {
-        res.status(404).json('error: todo id not found');
-    }*/
 
 })
 
 app.put('/todos/:id', function (req, res) {
     var todoid = parseInt(req.params.id, 10)
-    
+
     var body = _.pick(req.body, 'description', 'completed')
     var attributes = {};
-   
 
-    if (body.hasOwnProperty('completed') ) {
+
+    if (body.hasOwnProperty('completed')) {
         attributes.completed = body.completed;
-    } 
+    }
     if (body.hasOwnProperty('description')) {
         attributes.description = body.description;
-    } 
-    db.todo.findById(todoid).then(function(todo){
-        if(todo){
-            todo.update(attributes).then(function(todo){
-            res.json(todo.toJSON())
-        },function(e){
+    }
+    db.todo.findById(todoid).then(function (todo) {
+        if (todo) {
+            todo.update(attributes).then(function (todo) {
+                res.json(todo.toJSON())
+            }, function (e) {
                 res.status(400).json(e)
             })
-        }else{
+        } else {
             res.status(404).send()
         }
-    }, function(){
+    }, function () {
         res.status(500).send()
     })
 })
