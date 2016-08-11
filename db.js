4c3530395b86cdb52fe6c47eb7b7c4b90fa08ac0
khawar ali <khawarali971@gmail.com>
2016-08-11 19:27:25 +0500

    association

--- db.js
+++ db.js
@@ -17,5 +17,6 @@ db.todo = sequelize.import(__dirname + '/models/todo.js')
 db.user = sequelize.import(__dirname + '/models/user.js')
 db.sequalize = sequelize;
 db.Sequalize = Sequelize;
-
+db.todo.belongsTo(db.user)
+db.user.hasMany(db.todo)
 module.exports = db;
