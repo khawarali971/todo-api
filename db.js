7778dfe4dd7ee7a8f0aa6887d8217990cf95e6a8
khawar ali <khawarali971@gmail.com>
2016-08-13 13:48:51 +0500

    user logout

--- db.js
+++ db.js
@@ -15,6 +15,7 @@ var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js')
 db.user = sequelize.import(__dirname + '/models/user.js')
+db.token = sequelize.import(__dirname + '/models/token.js')
 db.sequalize = sequelize;
 db.Sequalize = Sequelize;
 db.todo.belongsTo(db.user)
--- server.js
+++ server.js
@@ -135,22 +135,31 @@ app.post('/users', function (req, res) {
         console.log(e)
     })
 })
+app.delete('/users/login', middleware.requireAuthentication, function (req, res){
+    req.token.destroy().then(function(){
+        res.status(200).send()
+    }).catch( function () {
+        res.status(500).send()
+    })
+})
 app.post('/users/login', function (req, res) {
     var body = _.pick(req.body, 'email', 'password')
-
+    var userInstance;
     db.user.authenticate(body).then(function (user) {
         var token = user.generateToken('authentication');
-        if (token) {
-            res.header('Auth', token).json(user.toPublicJSON())
-        } else {
-            res.status(401).send()
-        }
-    }, function () {
+        userInstance = user;
+        return db.token.create({
+            token : token
+        })
+       
+    }).then(function(tokenInstance){
+        res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON())
+    }).catch( function () {
         res.status(401).send()
     })
 
 })
-db.sequalize.sync({ force: true}).then(function () {
+db.sequalize.sync().then(function () {
     app.listen(PORT, function () {
         console.log('server running on ' + PORT)
     })
